9b4b84f6912eabd51e3f899b6a33886263284171
anthony-y-zhu14 <41710998+anthony-y-zhu14@users.noreply.github.com>
2022-11-20 10:02:28 -0500

    [code]: shuffle and build card stack

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,10 +1,17 @@
 package util;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
+import config.GameConfig;
 import model.Card;
 
 public class Util {
   public static Stack<Card> shuffleAndBuildCardsStack () {
-    return new Stack<Card>();
+    return new Stack<Card>(){{
+      List<Card> cards = GameConfig.CARDS;
+      Collections.shuffle(cards);
+      addAll(cards);
+    }};
   }
 }
