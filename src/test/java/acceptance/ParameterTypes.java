b8942dc1de3014a3a30070e5392fb6f638d709de
anthony-y-zhu14 <41710998+anthony-y-zhu14@users.noreply.github.com>
2022-11-24 19:22:50 -0500

    [refactor]: move some hashmap to be static final

--- src/test/java/acceptance/ParameterTypes.java
+++ src/test/java/acceptance/ParameterTypes.java
@@ -6,57 +6,57 @@ import constant.CardSuit;
 import io.cucumber.java.ParameterType;
 
 public class ParameterTypes {
+  public static final HashMap<String, String> CARD_SUIT_MAP = new HashMap<>(){{
+    put("C", CardSuit.CLUBS);
+    put("D", CardSuit.DIAMONDS);
+    put("H", CardSuit.HEARTS);
+    put("S", CardSuit.SPADES);
+  }};
+
+  public static final HashMap<String, String> CARD_VALUE_MAP = new HashMap<>(){{
+    put("A", constant.CardValue.A);
+    put("2", constant.CardValue.TWO);
+    put("3", constant.CardValue.THREE);
+    put("4", constant.CardValue.FOUR);
+    put("5", constant.CardValue.FIVE);
+    put("6", constant.CardValue.SIX);
+    put("7", constant.CardValue.SEVEN);
+    put("8", constant.CardValue.EIGHT);
+    put("9", constant.CardValue.NINE);
+    put("10", constant.CardValue.TEN);
+    put("J", constant.CardValue.JACK);
+    put("Q", constant.CardValue.QUEEN);
+    put("K", constant.CardValue.KING);
+  }};
+
+  public static final HashMap<String, String> DIRECTION_MAP = new HashMap<>(){{
+    put("normal", Direction.NORMAL);
+    put("opposite", Direction.REVERSE);
+  }};
+
+  public static final HashMap<String, Boolean> OUTCOME_MAP = new HashMap<>(){{
+    put("success", true);
+    put("failure", false);
+  }};
+
   // == Parameter Type =======================
   @ParameterType(".*")
   public String CardSuit (String suit) {
-    HashMap<String, String> cardSuitMap = new HashMap<>(){{
-      put("C", CardSuit.CLUBS);
-      put("D", CardSuit.DIAMONDS);
-      put("H", CardSuit.HEARTS);
-      put("S", CardSuit.SPADES);
-    }};
-    
-    return cardSuitMap.get(suit);
+    return CARD_SUIT_MAP.get(suit);
   }
 
   @ParameterType(".*")
   public String CardValue (String suit) {
-    HashMap<String, String> cardValueMap = new HashMap<>(){{
-      put("A", constant.CardValue.A);
-      put("2", constant.CardValue.TWO);
-      put("3", constant.CardValue.THREE);
-      put("4", constant.CardValue.FOUR);
-      put("5", constant.CardValue.FIVE);
-      put("6", constant.CardValue.SIX);
-      put("7", constant.CardValue.SEVEN);
-      put("8", constant.CardValue.EIGHT);
-      put("9", constant.CardValue.NINE);
-      put("10", constant.CardValue.TEN);
-      put("J", constant.CardValue.JACK);
-      put("Q", constant.CardValue.QUEEN);
-      put("K", constant.CardValue.KING);
-    }};
-    
-    return cardValueMap.get(suit);
+    return CARD_VALUE_MAP.get(suit);
   }
 
   @ParameterType(".*")
   public String Direction (String direction) {
-    HashMap<String, String> directionMap = new HashMap<>(){{
-      put("normal", Direction.NORMAL);
-      put("opposite", Direction.REVERSE);
-    }};
-
-    return directionMap.get(direction);
+    return DIRECTION_MAP.get(direction);
   }
 
   @ParameterType(".*")
   public boolean Outcome (String outcome) {
-    HashMap<String, Boolean> directionMap = new HashMap<>(){{
-      put("success", true);
-      put("failure", false);
-    }};
-
-    return directionMap.get(outcome);
+    return OUTCOME_MAP.get(outcome);
   }
 }
